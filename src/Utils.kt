dbc4542176176511666954710d9aca588443821a
daniel <daniel.huebscher@mariz.ch>
2023-12-02 17:28:22 +0100

    Enhance Utils.kt

--- src/Utils.kt
+++ src/Utils.kt
@@ -19,3 +19,13 @@ fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteA
  * The cleaner shorthand for printing output.
  */
 fun Any?.println() = println(this)
+
+fun checkResult(result: Int, expectedValue: Int) {
+    println(result)
+    check(result == expectedValue)
+}
+
+fun checkResult(result: String, expectedValue: String) {
+    println(result)
+    check(result == expectedValue)
+}
