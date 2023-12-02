c0a381c9bc4b013670042b4cb67a0152a02d86f6
daniel <daniel.huebscher@mariz.ch>
2023-12-02 17:28:43 +0100

    Day01, exercise 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,23 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+import java.io.File
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+fun main() {
+    val testFile = "Day01_test"
+    val file = "Day01"
+
+    fun part1(input: List<String>): Int = input
+        .sumOf { line -> line.first { it.isDigit() }.digitToInt() }
+        .times(10)
+        .plus(input.sumOf { line -> line.last { it.isDigit() }.digitToInt() })
+
+    // fun part2(input: List<String>): Int {
+    //    return input.size
+    // }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput(testFile)
+    checkResult(part1(testInput), 142)
 
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    val input = readInput(file)
+    checkResult(part1(input), 54561)
+    // checkResult(part2(input), 0)
 }
