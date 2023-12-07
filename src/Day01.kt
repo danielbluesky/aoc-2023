729146f567c6644cadd4a81e1888e1ce8526a4ac
daniel <daniel.huebscher@mariz.ch>
2023-12-07 20:42:32 +0100

    Day01, exercise 2, input Amela

--- src/Day01.kt
+++ src/Day01.kt
@@ -5,14 +5,22 @@ fun main() {
     val testInput2 = readInput("Day01_test_2")
     val input = readInput("Day01")
 
-    fun part1(input: List<String>) = input
-        .sumOf { line -> line.first { it.isDigit() }.digitToInt() }
-        .times(10)
-        .plus(input.sumOf { line -> line.last { it.isDigit() }.digitToInt() })
+    fun part1(input: List<String>) =
+        input.sumOf { line -> line.first { it.isDigit() }.digitToInt() }.times(10)
+            .plus(input.sumOf { line -> line.last { it.isDigit() }.digitToInt() })
 
     fun part2(input: List<String>) = part1(input.refine())
 
+    fun part1b(input: List<String>) = input
+        .sumOf { line ->
+            line.filter { it.isDigit() }.let {
+                it.first().digitToInt() * 10 + it.last().digitToInt()
+            }
+        }
+
     // test if implementation meets criteria from the description, like:
+    // checkResult(part1b(testInput1), 142)
+    // checkResult(part2(testInput2), 281)
     checkResult(part1(testInput1), 142)
     checkResult(part1(input), 54561)
     checkResult(part2(testInput2), 281)
@@ -26,6 +34,16 @@ fun String.replaceText(mapping: Map<String, String>): String =
 
 fun List<String>.refine() = this
     .map { line -> line.replaceText(mappingRules) }
+    .toList()
+
+fun List<String>.refine2() = this
+    .map { line ->
+        var newLine = line
+        mappingRules.forEach { (k, v) ->
+            newLine = newLine.replace(k, v)
+        }
+        newLine
+    }
     .toList()
 
 val mappingRules = mapOf(
