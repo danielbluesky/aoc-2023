c94ef98dbc0fe7193a4dbb3f72eed31bf6334a5c
daniel <daniel.huebscher@mariz.ch>
2023-12-08 16:18:00 +0100

    Day03, exercise 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -8,7 +8,7 @@ data class Quadruple(val first: Int, val second: Int, val third: Int, val fourth
 
 fun main() {
     val testInput1 = readInput("Day03_test_1")
-    // val testInput2 = readInput("Day03_test_2")
+    val testInput2 = readInput("Day03_test_2")
     val input = readInput("Day03")
 
     fun part1(input: List<String>): Int = input
@@ -16,14 +16,21 @@ fun main() {
         .findPartNumbers()
         .sum()
 
-    // fun part2(input: List<String>) = input
-    //    .size
+    fun part2(input: List<String>): Int {
+        val x = input
+            .parse()
+            .findGears()
+            .gearRatios()
+            .sum()
+        println(x)
+        return x
+    }
 
     // test if implementation meets criteria from the description, like:
     checkResult(part1(testInput1), 4361)
     checkResult(part1(input), 527144)
-    // checkResult(part2(testInput2), 0)
-    // checkResult(part2(input), 0)
+    checkResult(part2(testInput2), 467835)
+    checkResult(part2(input), 81463996)
 }
 
 fun Input.parse(): Pair<NumberRanges, SymbolCoords> {
@@ -42,7 +49,7 @@ fun Input.parse(): Pair<NumberRanges, SymbolCoords> {
     return Pair(numbers, symbols)
 }
 
-fun Pair<NumberRanges, SymbolCoords>.findPartNumbers(): MutableList<Int> {
+fun Pair<NumberRanges, SymbolCoords>.findPartNumbers(): List<Int> {
     val partNumbers: MutableList<Int> = mutableListOf()
     this.first.forEach { (number, line, start, end) ->
         if (this.second.any { (x, y) ->
@@ -52,5 +59,27 @@ fun Pair<NumberRanges, SymbolCoords>.findPartNumbers(): MutableList<Int> {
             partNumbers.add(number)
         }
     }
-    return partNumbers
+    return partNumbers.toList()
+}
+
+fun Pair<NumberRanges, SymbolCoords>.findGears(): Map<Pair<Int, Int>, MutableList<Int>> {
+    val gears = mutableMapOf<Pair<Int, Int>, MutableList<Int>>()
+    this.second.forEach { (x, y) ->
+        val numbers = mutableListOf<Int>()
+        this.first.forEach { (number, line, start, end) ->
+            if (x in (start - 1..end + 1) && y in (line - 1..line + 1)) {
+                numbers.add(number)
+            }
+        }
+        gears[Pair(x, y)] = numbers
+        }
+    return gears.toMap()
+}
+
+fun Map<Pair<Int, Int>, List<Int>>.gearRatios(): List<Int> {
+    val gearRatios = mutableListOf<Int>()
+    this.forEach {
+        if (it.value.size == 2) gearRatios.add(it.value.first() * it.value.last())
+    }
+    return gearRatios.toList()
 }
