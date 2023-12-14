6e7fc4a61372fab36a32b8ff6bf33e392ebbe5e1
daniel <daniel.huebscher@mariz.ch>
2023-12-14 19:59:59 +0100

    Day04, review comments

--- src/Day04.kt
+++ src/Day04.kt
@@ -3,6 +3,7 @@ import kotlin.math.pow
 
 data class Deck(val currentInstances: Int, val myNumbers: Set<Int>, val benchmark: Set<Int>)
 data class Result(val wonInstances: Int, val deckUpdates: Int)
+data class Deck2pra(val currentInstances: Int, val deckUpdates: Int)
 
 fun main() {
     val testInput1 = readInput("Day04_test_1")
@@ -19,6 +20,11 @@ fun main() {
     fun part2(input: List<String>) = input
         .parse2()
         .play()
+        .sumOf { it.currentInstances }
+
+    fun part2pra(input: List<String>) = input
+        .parse2pra()
+        .play2pra()
         .sumOf { it.currentInstances }
 
     // test if implementation meets criteria from the description, like:
@@ -48,8 +54,16 @@ fun List<String>.parse2(): MutableMap<Int, Deck> =
         round to Deck(1, firstSet, secondSet)
     }.toMutableMap()
 
+fun List<String>.parse2pra(): MutableMap<Int, Deck2pra> =
+    associate { line ->
+        val card = line.substringBefore(":").substringAfter(" ").trim().toInt()
+        val (firstSet, secondSet) = line.substringAfter(":").split(" | ").map { it.parseSets() }
+        card to Deck2pra(1, firstSet.intersect(secondSet).size) // die intersection per card(id) ist immer gleich
+    }.toMutableMap()
+
+// not directly working on MutableMap to avoid ConcurrentModificationException
 fun MutableMap<Int, Deck>.play(): List<Deck> {
-    (1..size).map { round -> // not directly working on MutableMap to avoid ConcurrentModificationException
+    (1..size).map { round ->
         val thisRound = this.entries.first { it.key == round }
         val result = thisRound.evaluate()
         (1..result.deckUpdates).map { i ->
@@ -57,6 +71,19 @@ fun MutableMap<Int, Deck>.play(): List<Deck> {
             val nextDeck = this[nextRound]!!
             val updatedNextDeck = Deck((nextDeck.currentInstances + result.wonInstances), nextDeck.myNumbers, nextDeck.benchmark)
             this.plusAssign(nextRound to updatedNextDeck)
+        }
+    }
+    return this.values.toList()
+}
+
+// not directly working on MutableMap to avoid ConcurrentModificationException
+fun MutableMap<Int, Deck2pra>.play2pra(): List<Deck2pra> {
+    (1..size).map { card ->
+        val thisCard = this.entries.first { it.key == card }
+        (1..thisCard.value.deckUpdates).map { i ->
+            val nextCard = thisCard.key + i
+            val nextDeck = this[nextCard]!!
+            this[nextCard] = Deck2pra((nextDeck.currentInstances + thisCard.value.currentInstances), nextDeck.deckUpdates)
         }
     }
     return this.values.toList()
