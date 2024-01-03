4ac7d64a223e367d4129be58ab2bb458ba0e7142
daniel <daniel.huebscher@mariz.ch>
2024-01-03 22:49:57 +0100

    Day07: Exercise 1, refactored

--- src/Day07.kt
+++ src/Day07.kt
@@ -18,7 +18,7 @@ fun main() {
     // test if implementation meets criteria from the description, like:
     checkResult(part1(testInput1), 6440)
     checkResult(part1(input), 247815719)
-    // checkResult(part2(testInput2), 0)
+    // checkResult(part2(testInput2), 5905)
     // checkResult(part2(input), 0)
 }
 
@@ -29,12 +29,8 @@ typealias EvaluatedHands = List<Int>
 fun List<String>.parse(): List<Pair<Hand, Bid>> = this
     .map { it.substringBefore(" ") to it.substringAfter(" ").toInt() }
 
-fun Pair<Hand, Bid>.evaluateHand(): Pair<EvaluatedHands, Bid> {
-    val counts = mutableMapOf<Char, Int>()
-    first.forEach { card -> counts[card] = counts.getOrDefault(card, 0) + 1 }
-    val evaluatedHand = mutableListOf(counts.handValue()) + first.map { c -> mapping[c]!! }.toMutableList()
-    return evaluatedHand to second
-}
+fun Pair<Hand, Bid>.evaluateHand(): Pair<EvaluatedHands, Bid> =
+    mutableListOf(first.groupingBy { it }.eachCount().handValue()) + first.map { mapping[it]!! } to second
 
 fun Map<Char, Int>.handValue() = when (this.values.sortedDescending()) {
     listOf(5) -> 6 // five of a kind
@@ -48,19 +44,11 @@ fun Map<Char, Int>.handValue() = when (this.values.sortedDescending()) {
 
 fun List<Pair<EvaluatedHands, Bid>>.sort(): List<Pair<EvaluatedHands, Bid>> = this
     .sortedWith(
-        compareBy<Pair<List<Int>, Int>> { it.first[0] }
-        .thenBy { it.first[1] }
-        .thenBy { it.first[2] }
-        .thenBy { it.first[3] }
-        .thenBy { it.first[4] }
-        .thenBy { it.first[5] },
+        compareBy({ it.first[0] }, { it.first[1] }, { it.first[2] }, { it.first[3] }, { it.first[4] }, { it.first[5] }),
     )
 
-fun List<Pair<EvaluatedHands, Bid>>.winnings(): List<Int> {
-    val winnings = mutableListOf<Int>()
-    this.mapIndexed { index, pair -> winnings += pair.second * (index + 1) }
-    return winnings.toList()
-}
+fun List<Pair<EvaluatedHands, Bid>>.winnings() = this
+    .mapIndexed { index, pair -> pair.second * (index + 1) }
 
 val mapping = mapOf(
     'A' to 14,
@@ -76,4 +64,4 @@ val mapping = mapOf(
     '4' to 4,
     '3' to 3,
     '2' to 2,
-    )
+)
