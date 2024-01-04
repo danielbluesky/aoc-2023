00a69ac2213e24c5b4f5d5d7dbdfba7d9ebe7dda
daniel <daniel.huebscher@mariz.ch>
2024-01-04 13:16:51 +0100

    Day07: Exercise 2

--- src/Day07.kt
+++ src/Day07.kt
@@ -13,18 +13,24 @@ fun main() {
         .sumOf { it }
 
     fun part2(input: List<String>) = input
-        .size
+        .parse()
+        .map { it -> it.evaluateHand2() }
+        .sort()
+        .winnings()
+        .sumOf { it }
 
     // test if implementation meets criteria from the description, like:
     checkResult(part1(testInput1), 6440)
     checkResult(part1(input), 247815719)
-    // checkResult(part2(testInput2), 5905)
-    // checkResult(part2(input), 0)
+    checkResult(part2(testInput2), 5905)
+    checkResult(part2(input), 248747492)
 }
 
 typealias Bid = Int
 typealias Hand = String
 typealias EvaluatedHands = List<Int>
+
+// Exercise 1
 
 fun List<String>.parse(): List<Pair<Hand, Bid>> = this
     .map { it.substringBefore(" ") to it.substringAfter(" ").toInt() }
@@ -55,6 +61,38 @@ val mapping = mapOf(
     'K' to 13,
     'Q' to 12,
     'J' to 11,
+    'T' to 10,
+    '9' to 9,
+    '8' to 8,
+    '7' to 7,
+    '6' to 6,
+    '5' to 5,
+    '4' to 4,
+    '3' to 3,
+    '2' to 2,
+)
+
+// Exercise 2
+fun Pair<Hand, Bid>.evaluateHand2(): Pair<EvaluatedHands, Bid> =
+    mutableListOf(first.groupingBy { it }.eachCount().handValue2(first.jokers())) + first.map { mapping2[it]!! } to second
+
+fun Hand.jokers() = this.count { it == 'J' }
+
+fun Map<Char, Int>.handValue2(jokers: Int) = when (this.values.sortedDescending()) {
+    listOf(5) -> 6 // five of a kind
+    listOf(4, 1) -> { if (jokers == 0) 5 else 6 } // four of a kind
+    listOf(3, 2) -> { if (jokers == 0) 4 else 6 } // full house
+    listOf(3, 1, 1) -> { if (jokers == 0) 3 else 5 } // three of a kind
+    listOf(2, 2, 1) -> { if (jokers == 0) 2 else if (jokers == 1) 4 else 5 } // two pairs
+    listOf(2, 1, 1, 1) -> { if (jokers == 0) 1 else 3 } // one pair
+    else -> { if (jokers == 0) 0 else 1 } // high card
+}
+
+val mapping2 = mapOf(
+    'A' to 14,
+    'K' to 13,
+    'Q' to 12,
+    'J' to 1,
     'T' to 10,
     '9' to 9,
     '8' to 8,
