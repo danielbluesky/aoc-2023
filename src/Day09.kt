9c292aa41e2ce2682e1e7eb62823ccd486a4b01a
daniel <daniel.huebscher@mariz.ch>
2024-01-06 20:22:02 +0100

    Day09: Exercise 2

--- src/Day09.kt
+++ src/Day09.kt
@@ -1,23 +1,22 @@
 // https://adventofcode.com/2023/day/9
 
 fun main() {
-    val testInput1 = readInput("Day09_test_1")
-    val testInput2 = readInput("Day09_test_1")
+    val testInput1 = readInput("Day09_test")
+    val testInput2 = readInput("Day09_test")
     val input = readInput("Day09")
 
-    fun part1(input: List<String>) = input
-            .parse()
-            .extrapolate()
-            .sum()
+    fun part1(input: List<String>, reversed: Boolean) = input
+        .parse()
+        .extrapolate(reversed)
+        .sum()
 
-    fun part2(input: List<String>) = input
-        .size
+    fun part2(input: List<String>, reversed: Boolean) = part1(input, reversed)
 
     // test if implementation meets criteria from the description, like:
-    checkResult(part1(testInput1), 114)
-    checkResult(part1(input), 2005352194)
-    // checkResult(part2(testInput2), 0)
-    // checkResult(part2(input), 0)
+    checkResult(part1(testInput1, false), 114)
+    checkResult(part1(input, false), 2005352194)
+    checkResult(part2(testInput2, true), 2)
+    checkResult(part2(input, true), 1077)
 }
 
 typealias Input = List<String>
@@ -25,10 +24,11 @@ typealias Parsed = List<List<Int>>
 
 fun Input.parse() = map { it.split(" ").map { it.toInt() } }
 
-fun Parsed.extrapolate(): List<Int> {
+fun Parsed.extrapolate(reversed: Boolean): List<Int> {
     val extrapolatedValues = mutableListOf<Int>()
-    this.map { initialList ->
+    this.map { input ->
         val deltas = mutableListOf<Int>()
+        val initialList = if (reversed) input.reversed() else input
         var deltaList = initialList
         while (deltaList.all { it == 0 }.not()) {
             deltaList = deltaList.zipWithNext().map { it.second - it.first }
